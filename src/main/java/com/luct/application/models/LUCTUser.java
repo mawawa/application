c0fd264b56d36636634554a23541c140e9bce6f4
wandile <nanashongwe@gmail.com>
2025-04-28 14:52:45 +0200

    initial

---
+++ src/main/java/com/luct/application/models/LUCTUser.java
@@ -0,0 +1,41 @@
+package com.luct.application.models;
+
+@Entity
+public class LUCTUser {
+    private String emailAddress;
+    private String password;
+    private String fullName;
+    private String confirmPassword;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
